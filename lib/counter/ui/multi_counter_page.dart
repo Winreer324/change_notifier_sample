778a5ccb323af8c085b79529fe70f1519fce5420
IgorZlobin <32igor3@mail.ru>
2021-12-21 16:56:46 +0300

    add sample selector+ValueListenableProvider

--- lib/counter/ui/multi_counter_page.dart
+++ lib/counter/ui/multi_counter_page.dart
@@ -1,5 +1,6 @@
 import 'package:change_notifier_sample/counter/model/counter_notifier.dart';
 import 'package:change_notifier_sample/helper_widgets/helper_provider_widget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,9 +12,12 @@ class MultiCounterPage extends StatefulWidget {
 }
 
 class _CounterPageState extends State<MultiCounterPage> with AutomaticKeepAliveClientMixin<MultiCounterPage> {
+  final _counterValueListenableProvider = ValueNotifier(0);
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -25,6 +29,13 @@ class _CounterPageState extends State<MultiCounterPage> with AutomaticKeepAliveC
           ChangeNotifierProvider<CounterNotifier>(
             create: (context) => CounterNotifier(),
             child: const _CounterItemWrapHelper(),
+          ),
+          ChangeNotifierProvider<CounterNotifier>(
+            create: (context) => CounterNotifier(),
+            child: const SelectorCounterWidget(),
+          ),
+          ValueListenableProviderWidget(
+            counter: _counterValueListenableProvider,
           ),
         ],
       ),
@@ -49,7 +60,7 @@ class __CounterItemState extends State<_CounterItemExtension> {
       children: [
         Consumer<CounterNotifier>(
           builder: (context, counter, child) {
-            return Center(child: Text("count: ${counter.counter}"));
+            return Center(child: Text("count (Consumer): ${counter.counter}"));
           },
         ),
         ElevatedButton(
@@ -78,13 +89,71 @@ class __CounterItemWrapHelper extends State<_CounterItemWrapHelper>
       children: [
         Consumer<CounterNotifier>(
           builder: (context, counter, child) {
-            return Center(child: Text("count: ${counter.counter}"));
+            return Center(child: Text("count (Consumer): ${counter.counter}"));
           },
         ),
         ElevatedButton(
           onPressed: provider.increment,
           child: const Icon(Icons.add),
         ),
+      ],
+    );
+  }
+}
+
+class SelectorCounterWidget extends StatefulWidget {
+  const SelectorCounterWidget({Key? key}) : super(key: key);
+
+  @override
+  _SelectorCounterWidgetState createState() => _SelectorCounterWidgetState();
+}
+
+class _SelectorCounterWidgetState extends State<SelectorCounterWidget>
+    with HelperProviderStatefulWidget<CounterNotifier, SelectorCounterWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Selector<CounterNotifier, String>(
+          builder: (context, counter, child) {
+            return Center(child: Text("count (Selector): $counter"));
+          },
+          selector: (BuildContext context, CounterNotifier counter) => counter.counter.toString(),
+        ),
+        ElevatedButton(
+          onPressed: provider.increment,
+          child: const Icon(Icons.add),
+        ),
+      ],
+    );
+  }
+}
+
+class ValueListenableProviderWidget extends StatefulWidget {
+  final ValueNotifier<int> counter;
+
+  const ValueListenableProviderWidget({Key? key, required this.counter}) : super(key: key);
+
+  @override
+  _ValueListenableProviderWidgetState createState() => _ValueListenableProviderWidgetState();
+}
+
+class _ValueListenableProviderWidgetState extends State<ValueListenableProviderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ValueListenableProvider<int>.value(
+          value: widget.counter,
+          child: Text('counter value listenable provider ${widget.counter.value}'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            widget.counter.value++;
+            setState(() {});
+          },
+          child: const Text('value listenable provider'),
+        )
       ],
     );
   }
